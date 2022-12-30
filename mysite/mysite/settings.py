a7a00e16e015c68d956b945a5b24700aba8f7e49
Lior Shahverdi <liorshahverdi@gmail.com>
2022-12-30 16:02:38 -0500

    adds redis channel layer.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -126,3 +126,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Daphne
 ASGI_APPLICATION = 'mysite.asgi.application'
+
+# Channels
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts":[("127.0.0.1", 6379)],
+        },
+    }
+}
